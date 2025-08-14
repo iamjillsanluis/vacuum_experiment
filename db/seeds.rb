fc9053dd51d7b8b494432cc2b128f2f36cc2b7d5
Jill San Luis <jill.sanluis@wealthsimple.com>
2025-08-14 15:52:08 -0400

    Observe if other records occupy the freed up pages from another table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,30 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-email_address = "me@example.com"
-client_app_name = "Vacuum Demo"
-if User.find_by(email_address: email_address).nil?
-  user = User.create!(email_address: email_address, password: "trustme")
-  client_app = Doorkeeper::Application.create(name: client_app_name, redirect_uri: "urn:ietf:wg:oauth:2.0:oob", scopes: ["read", "write"])
-else
-  user = User.find_by(email_address: email_address)
-  client_app = Doorkeeper::Application.find_by(name: client_app_name)
-end
+# email_address = "me@example.com"
+# client_app_name = "Vacuum Demo"
+# if User.find_by(email_address: email_address).nil?
+#   user = User.create!(email_address: email_address, password: "trustme")
+#   client_app = Doorkeeper::Application.create(name: client_app_name, redirect_uri: "urn:ietf:wg:oauth:2.0:oob", scopes: ["read", "write"])
+# else
+#   user = User.find_by(email_address: email_address)
+#   client_app = Doorkeeper::Application.find_by(name: client_app_name)
+# end
 
-1.upto(100000) do |i|
-  Doorkeeper::AccessToken.create(
-    resource_owner_id: user.id,
-    application_id: client_app.id,
-    refresh_token: SecureRandom.hex(32),
-    expires_in: 7200,
-    scopes: ''
-  )
+# 1.upto(5000) do |i|
+#   Doorkeeper::AccessToken.create(
+#     resource_owner_id: user.id,
+#     application_id: client_app.id,
+#     refresh_token: SecureRandom.hex(32),
+#     expires_in: 7200,
+#     scopes: ''
+#   )
 
-  if i % 1000 == 0
-    puts "Created #{i} access tokens"
-  end
+#   if i % 1000 == 0
+#     puts "Created #{i} access tokens"
+#   end
+# end
+
+1.upto(1000) do |i|
+  User.create!(email_address: "#{SecureRandom.hex(16)}@example.com", password: "trustme")
 end
